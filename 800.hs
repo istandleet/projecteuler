f211e3bceb4f8d59d0852d105a62c3978f6e5655
Robert Kennedy <robertkennedy@clearwateranalytics.com>
2022-06-01 18:12:20 -0700

    [800] [performance]

--- 800.hs
+++ 800.hs
@@ -1,21 +1,15 @@
 {-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE TupleSections #-}
 module Main where
 
-import Data.Function
-import Data.Foldable
 import qualified Data.List
-import qualified Data.List.Ordered as OL
-import qualified Data.Set as Set
+import qualified Data.Sequence as Seq
 
 import qualified Math.NumberTheory.Primes as P
-import Math.NumberTheory.Roots -- cabal install integer-roots --lib
 
 
 main :: IO ()
-main = print $ answer 800800
+main = print $ answer' 800800
 
 answer :: Integer -> Int
 answer constant = length $ reducing pass' $ map (fromInteger . P.unPrime) $ P.primes
@@ -23,6 +17,17 @@ answer constant = length $ reducing pass' $ map (fromInteger . P.unPrime) $ P.pr
 
 pass c = let con = c * log c in \a b -> a * log b + b * log a < con
 
+reducing :: (t -> t -> Bool) -> [t] -> [(t, t)]
 reducing f [] = []
 reducing f [x] = []
 reducing f (p:ps) = let ps' = takeWhile (f p) ps in map (p,) ps' ++ reducing f ps'
+
+answer' :: Integer -> Int 
+answer' constant = reducingl f $ Seq.fromList $ takeWhile (f 2) $ map (fromInteger . P.unPrime) $ P.primes
+    where f = pass $ fromIntegral constant
+
+reducingl :: (a -> a -> Bool) -> Seq.Seq a -> Int
+reducingl f = go 0
+    where 
+    go !n Seq.Empty = n
+    go !n (p Seq.:<| ps) = let ps' = Seq.dropWhileR (not . f p) ps in go (n+length ps') ps'
