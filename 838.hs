a787c9fc291c6510496f69768c419f08a4270cad
Robert Kennedy <robertkennedy@clearwateranalytics.com>
2023-04-26 10:21:01 -0700

    [838] solved

--- 838.hs
+++ 838.hs
@@ -1,10 +1,4 @@
 
-{-
-    NB: This does not fully solve the problem. While `covercosts` appears to implement a correct
-    algorithm, `f` only works in the naive case, where you can greedily take the most common
-    remaining PF. 
--}
-
 {-# LANGUAGE TransformListComp #-}
 {-# LANGUAGE BangPatterns #-}
 module Main where
@@ -12,16 +6,26 @@ module Main where
 import qualified Data.List
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Data.Map (Map)
+import qualified Data.Map.Strict as Map
+import GHC.Exts (the, groupWith)
 
 import Math.NumberTheory.Primes -- arithmoi
 
-import GHC.Exts (the, groupWith)
+import Data.Time
 
 main :: IO ()
 main = print $ f (10^6)
 
+-- | Actual solution. Runs in ~1.5s in ghci
 f :: Integer -> Double
 f n = 
+    let (done, remnant) = requiredPrimes [3,13..n]
+     in logsum (map unPrime $ Set.toList done) + covercosts (map (Set.map unPrime) remnant)
+
+-- | Original solution. Works by coincidence for 10^6
+f' :: Integer -> Double
+f' n = 
     let (done, remnant) = requiredPrimes [3,13..n]
         naive = naiveCover remnant
      in logsum $ map unPrime $ Set.toList done ++ naive
@@ -39,6 +43,23 @@ requiredCoverElems ss =
         remnant = filter (Set.disjoint inits) bs
      in (inits, remnant)
 
+-- | Remove primes from your set which are strictly dominated. A prime is strictly dominated
+-- if it only occurs once and there is another element in the set which is lower than it.
+dominatedPrimes :: Ord a => [Set a] -> (Set a, [Set a])
+dominatedPrimes ss = if any (==1) counts then requiredCoverElems $ map filterDominanted ss else (mempty, ss)
+    where 
+    counts = Map.fromList $ count $ foldMap Set.toList ss
+    filterDominanted ps = Set.filter (\p -> p == minimum ps || counts Map.! p > 1) ps
+
+-- initialRequirements $ snd $ requiredPrimes [3,13..2800]
+initialRequirements :: Ord a => [Set a] -> (Set a, [Set a])
+initialRequirements ss = 
+    let (ps, remnant) = requiredCoverElems ss
+        (dp, remnant') = dominatedPrimes remnant
+     in if null dp
+            then (ps, remnant')
+            else let (r, s) = initialRequirements remnant' in (ps <> dp <> r, s) -- this recursion is not actually required, since dominatedPrimes calls `requiredCoverElems`, which is ~idempotent. But that's just what's happening now, this is more future proof.
+
 covers, fullcovers :: (Foldable t, Ord a) => Set a -> t (Set a) -> Bool
 covers s = not . any (Set.disjoint s)
 fullcovers s is = s `covers` is && not (any (\n -> covers (Set.delete n s) is) s) 
@@ -47,21 +68,24 @@ toprimes :: Integer -> Set (Prime Integer)
 toprimes = Set.fromList . map fst . factorise
 
 -- covercosts (map (Set.map unPrime) $ snd $ requiredPrimes [3,13..2800]) == 13.22248559711485
+-- covercosts [Set.fromList [2,1000], Set.fromList [3,1000]] == 1.791759469228055
 covercosts :: [Set Integer] -> Double
 covercosts ss = 
-    let nc = naiveCover ss
-     in go [] ss (logsum nc) (Set.toList $ mconcat ss)
+    let (ps, remnant) = initialRequirements ss
+     in go (Set.toList ps) remnant (Set.toList $ mconcat remnant Set.\\ ps)
     where 
-    go req remnant !upperbound [] = logsum req `min` upperbound
-    go req [] !upperbound _ = logsum req `min` upperbound
-    go req remnant !upperbound _ 
+    upperbound = logsum $ naiveCover ss
+    go req remnant [] = logsum req `min` upperbound
+    go req [] _ = logsum req `min` upperbound
+    go req remnant ps
         | logsum req > upperbound = upperbound
         -- | logsum (req ++ lowerbound remnant) > upperbound = upperbound
-    go req remnant !upperbound (banned:cs) = 
+    go req remnant (banned:cs) = 
         let -- find all the places where you now have a forced decision
-            (newps, togo) = requiredCoverElems $ map (Set.delete banned) remnant 
-            ifRequired = go (banned:req) (filter (Set.notMember banned) remnant) upperbound cs
-            ifSkipped = go (Set.toList newps <> req) (filter (Set.disjoint newps) togo) upperbound cs
+            (newps, togo) = requiredCoverElems $ map (Set.delete banned) remnant
+
+            ifRequired = go (banned:req) (filter (Set.notMember banned) remnant) cs
+            ifSkipped = go (Set.toList newps <> req) togo cs
 
             lbifskipped = logsum 
                        $ req 
@@ -69,15 +93,6 @@ covercosts ss =
                       <> lowerbound togo
 
          in if lbifskipped > upperbound then ifRequired else min ifRequired ifSkipped
-         
-forcedCover :: Double -> Integer -> [Set Integer] -> Maybe [Set Integer]
-forcedCover target c remnant =
-    -- first, determine if c is even possible, by finding forced moves
-    let (newps, togo) = requiredCoverElems $ map (Set.delete c) remnant
-        target' = logsum $ Set.toList newps 
-     in if target' > target 
-         then Just $ filter (Set.notMember c) remnant
-         else Nothing
 
 naiveCover :: Ord a => [Set a] -> [a]
 naiveCover [] = []
@@ -104,3 +119,9 @@ lowerbound ss = go (length ss) $ zip ems $ Data.List.sortOn negate $  map snd $ 
 
 count :: Ord a => [a] -> [(a, Int)]
 count xs = [(the x, length x) | x <- xs, then group by x using groupWith]
+
+timeit x = do
+    a <- getCurrentTime
+    print x 
+    b <- getCurrentTime 
+    print (b `diffUTCTime` a)
